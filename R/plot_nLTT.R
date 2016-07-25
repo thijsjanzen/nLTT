d005c6d35dbf4fbc75f4ea1f6be25bb1de4ba01b
richelbilderbeek <richel@richelbilderbeek.nl>
2016-07-25 14:35:10 +0200

    Increased codecov, removed semicolons, remove typo

--- R/plot_nLTT.R
+++ R/plot_nLTT.R
@@ -24,11 +24,11 @@ nltt_plot <- function( phy, xlab = "Normalized Time",
   }
 
   #we use the ltt.plot.coords function from the package ape
-  xy <- ape::ltt.plot.coords( phy, backward = TRUE, tol = 1e-6);
-  xy[, 2] <- xy[, 2] / max(xy[, 2]); #normalize number lineages
+  xy <- ape::ltt.plot.coords( phy, backward = TRUE, tol = 1e-6)
+  xy[, 2] <- xy[, 2] / max(xy[, 2]) #normalize number lineages
 
-  xy[, 1] <- xy[, 1] + abs( min( xy[, 1])); #make sure time runs from 0..T
-  xy[, 1] <- xy[, 1] / max( xy[, 1]);      #normalize time
+  xy[, 1] <- xy[, 1] + abs( min( xy[, 1])) #make sure time runs from 0..T
+  xy[, 1] <- xy[, 1] / max( xy[, 1])      #normalize time
 
   plot.default(xy, xlab = xlab, ylab = ylab, xaxs = "r", yaxs = "r",
     type = "S", ...) #type = "S" ensures a stepwise function
@@ -50,14 +50,14 @@ nltt_lines <- function(phy, ...) {
 
   if (!inherits(phy, "phylo")) {
     # Stop imposed by ape::ltt.plot.coords
-    stop("nLTT.lines: ",
+    stop("nltt_lines: ",
       "phylogeny must be of class 'phylo', ",
       "but was of type '", class(phy), "' instead")
   }
   xy <- ape::ltt.plot.coords( phy, backward = TRUE, tol = 1e-6)
-  xy[, 2] <- xy[, 2] / max( xy[, 2]); #normalize number lineages
+  xy[, 2] <- xy[, 2] / max( xy[, 2]) #normalize number lineages
 
-  xy[, 1] <- xy[, 1] + abs( min( xy[, 1])); #make sure time runs from 0..T
-  xy[, 1] <- xy[, 1] / max( xy[, 1]);      #normalize time
+  xy[, 1] <- xy[, 1] + abs( min( xy[, 1])) #make sure time runs from 0..T
+  xy[, 1] <- xy[, 1] / max( xy[, 1])      #normalize time
   lines(xy, type = "S", ...)
 }
