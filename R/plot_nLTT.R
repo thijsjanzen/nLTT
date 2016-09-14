42314bc158502bb991f34818ea4d41f77c9d73a2
richelbilderbeek <richel@richelbilderbeek.nl>
2016-09-14 09:31:56 +0200

    Roxygenized docs of nllt_lines and nltt_plot

--- R/plot_nLTT.R
+++ R/plot_nLTT.R
@@ -12,7 +12,19 @@
 #
 #
 ################################################################################
-
+#' This function uses a modified version of the ltt.plot function from \code{"ape"} to plot the normalized number of lineages through normalized time, where the number of lineages is normalized by dividing by the number of tips of the tree, and the time is normalized by the total time between the most common recent ancestor and the present, such that t(MRCA) = 0 & t(present) = 1.
+#' @title Normalized version of the ape function ltt.plot
+#' @usage nltt_plot(phy, xlab = "Normalized Time", ylab = "Normalized Lineages", ...)
+#' @param phy an object of class \code{"phylo"};
+#' @param xlab a character string (or a variable of mode character)
+#'   giving the label for the \eqn{x}-axis (default is "Normalized Time").
+#' @param ylab a character string (or a variable of mode character)
+#'   giving the label for the \eqn{y}-axis (default is "Normalized Lineages").
+#' @param \dots further graphical arguments that can be passed to \code{plot()}
+#' @author Thijs Janzen
+#' @examples
+#'   data(exampleTrees)
+#'   nltt_plot(exampleTrees[[1]])
 nltt_plot <- function( phy, xlab = "Normalized Time",
   ylab = "Normalized Lineages", ...) {
 
@@ -45,7 +57,16 @@ nltt_plot <- function( phy, xlab = "Normalized Time",
 #
 #
 ################################################################################
-
+#' This is a modified version of the \code{ape} function ltt.lines: add the normalized Lineage-Through-Time statistic of a phylogenetic tree to an already existing plot
+#' @title Normalized version of the ape function ltt.lines.
+#' @usage nltt_lines(phy, ...)
+#' @param phy an object of class \code{"phylo"};
+#' @param \dots further graphical arguments that can be passed to \code{lines()}
+#' @author Thijs Janzen
+#' @examples
+#'   data(exampleTrees)
+#'   nltt_plot(exampleTrees[[1]])
+#'   nltt_lines(exampleTrees[[2]], lty=2)
 nltt_lines <- function(phy, ...) {
 
   if (!inherits(phy, "phylo")) {
