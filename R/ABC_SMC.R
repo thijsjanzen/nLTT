e769bcc811fcd16bddb74a16ab26d3c5b976e395
Thijs Janzen <janzen@evolbio.mpg.de>
2016-09-15 14:50:48 +0200

    Fixed typo in calculate_weight after failing test
    Those tests work!

--- R/ABC_SMC.R
+++ R/ABC_SMC.R
@@ -21,7 +21,7 @@ calculate_weight <- function(weights, particles,
   for ( i in seq_along( particles)) {
     vals[i] <- weights[i]
     for (j in seq_along( current)) {
-      diff <- log(current[j] - log(particles[[i]][j]))
+      diff <- log(current[j]) - log(particles[[i]][j])
       vals[i] = vals[i] * stats::dnorm(diff, mean = 0, sd = sigma)
     }
   }
--- tests/testthat/test_calculate_weight.R
+++ tests/testthat/test_calculate_weight.R
@@ -8,24 +8,24 @@ test_that("calculate_weight use", {
     particles[[i]] <- c(10, 10)
   }
   sigma <- 1
-  prior_density_function <- function(x) {
+  pdf <- function(x) {
     dunif(x[1], min = 0, max = 1000) *
     dunif(x[2], min = 0, max = 1000)
   }
 
   expect_equal(
     calculate_weight(weights, particles,
-      current = c(5, 5), sigma, prior_density_function),
+      current = c(5, 5), sigma, pdf),
     calculate_weight(weights, particles,
-      current = c(20, 20), sigma, prior_density_function),
+      current = c(20, 20), sigma, pdf),
     tolerance = 0.0001
   )
 
   expect_equal(
     calculate_weight(weights, particles,
-      current = c(5, 5), sigma, prior_density_function),
+      current = c(5, 5), sigma, pdf),
     calculate_weight(weights, particles,
-      current = c(5.0, 5.0), sigma, prior_density_function),
+      current = c(5.0, 5.0), sigma, pdf),
     tolerance = 0.0001
   )
 })
