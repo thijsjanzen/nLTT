d6b4acce6b325f4e33d550b05a0a888500fb95f5
richelbilderbeek <richel@richelbilderbeek.nl>
2017-01-20 04:00:25 +0100

    Progress

--- R/nLTTDiff.R
+++ R/nLTTDiff.R
@@ -111,6 +111,26 @@ nltt_diff_exact_brts <- function(
   return ( diff)
 }
 
+
+
+#' Calculates the exact difference between the nLTT
+#' curves of the branching times
+#' @author Thijs Janzen
+#' @param b_times branching times of the first phylogeny
+#' @param lineages the number of lineages, usually one to the number of lineages
+#' @param b_times_2 branching times of the first phylogeny
+#' @param lineages_2 the number of lineages, usually one to the number of lineages
+#' @param distance_method (string) absolute, or squared distance?
+#' @export
+nltt_diff_exact_norm_brts <- function(
+  b_times_N,
+  lineages_N,
+  b_times2_N,
+  lineages2_N,
+  distance_method)
+{
+}
+
 #' Calculates the exact difference between the lineage through time curves of tree1 & tree2 (normalized in time and for the number of lineages)
 #' @author Thijs Janzen
 #' @param tree1 (phylo) First phylogenetic tree
