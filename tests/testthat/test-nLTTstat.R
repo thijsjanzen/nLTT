20cebdffa1c4ff30d04cd5eeaff0fee1a7bc7827
richelbilderbeek <richel@richelbilderbeek.nl>
2017-01-24 08:15:53 +0100

    Removed duplicate tests, added new one

--- tests/testthat/test-nLTTstat.R
+++ tests/testthat/test-nLTTstat.R
@@ -1,6 +1,6 @@
 context("nLTTstat")
 
-test_that("nLTTstat use", {
+test_that("Identical trees have an nLTTstat of zero", {
 
   set.seed(314)
   p <- ape::rcoal(10)
@@ -14,28 +14,32 @@ test_that("nLTTstat use", {
     0.0, nLTTstat(tree1 = p, tree2 = p, distance_method = "squ"), # nolint nLTTstat should be all lowercase, left in for backwards compatibility
     tolerance = 0.0001
   )
+})
+
+
+
+test_that("abs nLTTstat on known tree", {
+
+  set.seed(314)
+  p <- ape::rcoal(10)
+  q <- ape::rcoal(10)
 
   expect_equal(
     0.1380347, nLTTstat(tree1 = p, tree2 = q, distance_method = "abs"), # nolint nLTTstat should be all lowercase, left in for backwards compatibility
     tolerance = 0.0001
   )
+})
+
+test_that("squ nLTTstat on known tree", {
+
+  set.seed(314)
+  p <- ape::rcoal(10)
+  q <- ape::rcoal(10)
 
   expect_equal(
-    nLTTstat(tree1 = p, tree2 = q, distance_method = "abs"), # nolint nLTTstat should be all lowercase, left in for backwards compatibility
-    nLTTstat(tree1 = p, tree2 = q, distance_method = "abs"),  # nolint nLTTstat should be all lowercase, left in for backwards compatibility
+    0.02407742, nLTTstat(tree1 = p, tree2 = q, distance_method = "squ"), # nolint nLTTstat should be all lowercase, left in for backwards compatibility
     tolerance = 0.0001
   )
-
-  expect_equal(
-    0.0, nLTTstat(tree1 = q, tree2 = q, distance_method = "abs"), # nolint nLTTstat should be all lowercase, left in for backwards compatibility
-    tolerance = 0.0001
-  )
-  expect_equal(
-    0.0, nLTTstat(tree1 = q, tree2 = q, distance_method = "squ"), # nolint nLTTstat should be all lowercase, left in for backwards compatibility
-    tolerance = 0.0001
-  )
-
-  # Cannot test for incorrect distance_method due to Issue #6
 })
 
 
