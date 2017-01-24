e1d11419df058c540910c51b6a42179bcc293a0d
richelbilderbeek <richel@richelbilderbeek.nl>
2017-01-24 16:50:22 +0100

    EOD

---
+++ R/get_norm_brts.R
@@ -0,0 +1,21 @@
+#' Collect the normalized branching times from the stem age
+#' @param phylogeny a phylogeny of class 'phylo'
+#' @return branching times, in time units before the present
+#' @examples
+#'   phylogeny <- ape::read.tree(text = "((a:2,b:2):1,c:3);")
+#'   phylogeny$root.edge <- 2
+#'   testthat::expect_true(
+#'     all(nLTT::get_branching_times(phylogeny) == c(5, 3, 2)))
+#' @author Richel Bilderbeek
+#' @export
+get_norm_brts <- function(phylogeny) {
+  brts <- get_branching_times(phylogeny)
+  # Repeat the last value to have te brts at the present
+  brts <- c(brts, utils::tail(brts, n = 1))
+  # Reverse to have 'times ago' (e.g. 4,3,0) -> (0, -1, -4) -> (0,1,4)
+  brts <- brts - brts[1]
+  brts <- -brts
+  # Normalize
+  brts <- brts / utils::tail(brts, n = 1)
+ return (brts)
+}
---
+++ R/get_norm_n.R
@@ -0,0 +1,21 @@
+#' Collect the normalized number of lineages from the stem age
+#' @param phylogeny a phylogeny of class 'phylo'
+#' @return branching times, in time units before the present
+#' @examples
+#'   phylogeny <- ape::read.tree(text = "((a:2,b:2):1,c:3);")
+#'   phylogeny$root.edge <- 2
+#'   testthat::expect_true(
+#'     all(nLTT::get_branching_times(phylogeny) == c(5, 3, 2)))
+#' @author Richel Bilderbeek
+#' @export
+get_norm_n <- function(phylogeny) {
+  ns <- get_n_lineages(phylogeny)
+  # Repeat the last value to have te ns at the present
+  ns <- c(ns, utils::tail(ns, n = 1))
+  # Reverse to have 'times ago' (e.g. 4,3,0) -> (0, -1, -4) -> (0,1,4)
+  ns <- ns - ns[1]
+  ns <- -ns
+  # Normalize
+  ns <- ns / utils::tail(ns, n = 1)
+ return (ns)
+}
