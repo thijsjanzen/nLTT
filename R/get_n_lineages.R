4a6d1662034b44bb5844540bf0e678df15f5ff2e
richelbilderbeek <richel@richelbilderbeek.nl>
2017-01-24 12:18:04 +0100

    Added get_n_lineages

---
+++ R/get_n_lineages.R
@@ -0,0 +1,22 @@
+#' Collect the number of lineages from the stem age
+#' @param phylogeny a phylogeny of class 'phylo'
+#' @return number of lineages, will go from 1 to the number of tips,
+#'   if there is a stem, will go from 2 to the number of tips
+#'   if there is no stem
+#' @examples
+#'   phylogeny <- ape::read.tree(text = "((a:2,b:2):1,c:3);")
+#'   testthat::expect_true(
+#'     all(nLTT::get_n_lineages(phylogeny) == c(2, 3)))
+#'   phylogeny$root.edge <- 2
+#'   testthat::expect_true(
+#'     all(nLTT::get_n_lineages(phylogeny) == c(1, 2, 3)))
+#' @author Richel Bilderbeek
+#' @export
+get_n_lineages <- function(phylogeny)
+{
+  if (!is.null(phylogeny$root.edge))
+  {
+    return (1:length(phylogeny$tip.label))
+  }
+  return (2:length(phylogeny$tip.label))
+}
