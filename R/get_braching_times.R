c4bd729f80bff20e55795545777d6b35071f875a
richelbilderbeek <richel@richelbilderbeek.nl>
2017-01-24 10:47:16 +0100

    Added get_branching_times, that also uses the stem

---
+++ R/get_braching_times.R
@@ -0,0 +1,21 @@
+#' Collect the branching times from the stem age
+#' @param phylogeny a phylogeny of class 'phylo'
+#' @return branching times, in time units before the present
+#' @examples
+#'   phylogeny <- ape::read.tree(text = "((a:2,b:2):1,c:3);")
+#'   phylogeny$root.edge <- 2
+#'   testthat::expect_true(
+#'     all(nLTT::get_branching_times(phylogeny) == c(5, 3, 2)))
+#' @author Richel Bilderbeek
+#' @export
+get_branching_times <- function(phylogeny)
+{
+  brts <- ape::branching.times(phylogeny)
+  if (!is.null(phylogeny$root.edge))
+  {
+    # Put the stem age first
+    return (c(brts[1] + phylogeny$root.edge, brts))
+  }
+  return (brts)
+}
+
---
+++ tests/testthat/test-get_branching_times.R
@@ -0,0 +1,26 @@
+context("get_branching_times")
+
+test_that("get_branching_times use", {
+  #
+  #         +---+---+---+ c
+  #         |
+  # +---+---+   +---+---+ b
+  #         |   |
+  #         +---+
+  #             |
+  #             +---+---+ a
+  #
+  #
+  # +---+---+---+---+---+ time (million years ago)
+  # 5   4   3   2   1   0
+  phylogeny <- ape::read.tree(text = "((a:2,b:2):1,c:3);"); phylogeny$root.edge <- 2
+  #ape::plot.phylo(phylogeny, root.edge = TRUE); ape::add.scale.bar()
+
+  # ape ignores the stem
+  testthat::expect_true(
+    all(ape::branching.times(phylogeny) == c(3, 2)))
+
+  # get_branching_times adds the stem
+  testthat::expect_true(
+    all(nLTT::get_branching_times(phylogeny) == c(5, 3, 2)))
+})
