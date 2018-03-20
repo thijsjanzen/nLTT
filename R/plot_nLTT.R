485c9d286ebadef165eefb01cc110681bc8e61a5
richelbilderbeek <richel@richelbilderbeek.nl>
2018-03-20 18:11:39 +0100

    Make @lintr-bot a bit happier

--- R/plot_nLTT.R
+++ R/plot_nLTT.R
@@ -44,7 +44,7 @@ nltt_plot <- function( phy, xlab = "Normalized Time",
   }
 
   #we use the ltt.plot.coords function from the package ape
-  xy <- ape::ltt.plot.coords( phy, backward = TRUE, tol = 1e-6)
+  xy <- ape::ltt.plot.coords(phy, backward = TRUE, tol = 1e-6)
   xy[, 2] <- xy[, 2] / max(xy[, 2]) #normalize number lineages
 
   xy[, 1] <- xy[, 1] + abs( min( xy[, 1])) #make sure time runs from 0..T
@@ -86,10 +86,10 @@ nltt_lines <- function(phy, ...) {
       "phylogeny must be of class 'phylo', ",
       "but was of type '", class(phy), "' instead")
   }
-  xy <- ape::ltt.plot.coords( phy, backward = TRUE, tol = 1e-6)
-  xy[, 2] <- xy[, 2] / max( xy[, 2]) #normalize number lineages
+  xy <- ape::ltt.plot.coords(phy, backward = TRUE, tol = 1e-6)
+  xy[, 2] <- xy[, 2] / max(xy[, 2]) #normalize number lineages
 
-  xy[, 1] <- xy[, 1] + abs( min( xy[, 1])) #make sure time runs from 0..T
-  xy[, 1] <- xy[, 1] / max( xy[, 1])      #normalize time
+  xy[, 1] <- xy[, 1] + abs(min( xy[, 1])) #make sure time runs from 0..T
+  xy[, 1] <- xy[, 1] / max(xy[, 1])      #normalize time
   graphics::lines(xy, type = "S", ...)
 }
