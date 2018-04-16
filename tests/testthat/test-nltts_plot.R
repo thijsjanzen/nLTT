0988853a30208cafc2cfea8e8be6146182c3eb75
richelbilderbeek <richel@richelbilderbeek.nl>
2018-04-16 13:31:04 +0200

    Fix #38

--- tests/testthat/test-nltts_plot.R
+++ tests/testthat/test-nltts_plot.R
@@ -8,21 +8,6 @@ test_that("use", {
   testthat::expect_silent(nltts_plot(phylos))
 })
 
-test_that("use, #38", {
-
-  # Works for 10 trees
-  set.seed(42)
-  n_tips <- 10
-  n_trees <- 10
-  phylos <- c(ape::rcoal(n = n_tips, N = n_trees))
-  testthat::expect_silent(nltts_plot(phylos))
-
-  # Fails for 11 trees
-  set.seed(42)
-  phylos <- c(ape::rcoal(n = n_tips, N = n_trees))
-  testthat::expect_silent(nltts_plot(phylogenies = phylos))
-})
-
 test_that("abuse", {
 
   testthat::expect_error(
