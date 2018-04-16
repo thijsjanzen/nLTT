596af33adbed1926848810663fca17d92251ed74
richelbilderbeek <richel@richelbilderbeek.nl>
2018-04-16 13:29:39 +0200

    No bug at #38

--- tests/testthat/test-nltts_plot.R
+++ tests/testthat/test-nltts_plot.R
@@ -3,23 +3,24 @@ context("nltts_plot")
 test_that("use", {
 
   set.seed(42)
-  phylos <- c(ape::rcoal(10))
+  n_tips <- 10
+  phylos <- c(ape::rcoal(n = n_tips))
   testthat::expect_silent(nltts_plot(phylos))
 })
 
 test_that("use, #38", {
 
-  skip("Expose #38")
-
   # Works for 10 trees
   set.seed(42)
-  phylos <- c(ape::rcoal(10, n = 10))
+  n_tips <- 10
+  n_trees <- 10
+  phylos <- c(ape::rcoal(n = n_tips, N = n_trees))
   testthat::expect_silent(nltts_plot(phylos))
 
   # Fails for 11 trees
   set.seed(42)
-  phylos <- c(ape::rcoal(10, n = 11))
-  testthat::expect_silent(nltts_plot(phylos))
+  phylos <- c(ape::rcoal(n = n_tips, N = n_trees))
+  testthat::expect_silent(nltts_plot(phylogenies = phylos))
 })
 
 test_that("abuse", {
