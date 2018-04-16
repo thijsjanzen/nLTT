f8931ce13829a10d996889034cd2a4345e7e96f5
richelbilderbeek <richel@richelbilderbeek.nl>
2018-04-16 13:25:23 +0200

    Expose #38

--- tests/testthat/test-nltts_plot.R
+++ tests/testthat/test-nltts_plot.R
@@ -2,7 +2,23 @@ context("nltts_plot")
 
 test_that("use", {
 
+  set.seed(42)
   phylos <- c(ape::rcoal(10))
+  testthat::expect_silent(nltts_plot(phylos))
+})
+
+test_that("use, #38", {
+
+  skip("Expose #38")
+
+  # Works for 10 trees
+  set.seed(42)
+  phylos <- c(ape::rcoal(10, n = 10))
+  testthat::expect_silent(nltts_plot(phylos))
+
+  # Fails for 11 trees
+  set.seed(42)
+  phylos <- c(ape::rcoal(10, n = 11))
   testthat::expect_silent(nltts_plot(phylos))
 })
 
