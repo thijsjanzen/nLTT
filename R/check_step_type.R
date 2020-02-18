9c3602c5bc11faed9e75660bc6c04132959e397e
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-18 12:42:02 +0100

    Thanks @lintr-bot

--- R/check_step_type.R
+++ R/check_step_type.R
@@ -4,7 +4,8 @@
 #' @inheritParams default_params_doc
 #' @export
 check_step_type <- function(step_type) {
-  if (length(step_type) != 1 || (step_type != "lower" && step_type != "upper")) {
+  if (length(step_type) != 1 ||
+      (step_type != "lower" && step_type != "upper")) {
     stop(
       "stretch_nltt_matrix: step_type must be either 'lower' or 'upper', ",
       "step_type supplied was '", step_type, "' instead"
