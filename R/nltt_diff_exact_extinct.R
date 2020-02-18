07c89f702d14d18e39c6967c8ac0e5015a81ca7b
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-18 11:31:18 +0100

    @Neves-P: cannot link to DAISIE functions, because nLTT does not (and should not) depend on DAISIE

--- R/nltt_diff_exact_extinct.R
+++ R/nltt_diff_exact_extinct.R
@@ -1,7 +1,7 @@
 #' Calculates the exact difference between the nLTT
 #' curves of the event times. This includes extinction events.
-#' @description Takes branching times such as (for example) as returned by a
-#' \code{\link[DAISIE]{DAISIE_sim()}} simulation.
+#' @description Takes branching times such as (for example) as returned by
+#' the \code{DAISIE_sim} of the \code{DAISIE} R package
 #' @author Pedro Neves and Richel Bilderbeek and Thijs Janzen
 #' @inheritParams default_params_doc
 #' @param event_times event times of the first phylogeny
